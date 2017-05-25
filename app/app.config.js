3068825c3efdc2e33cac4accac9a2f92dc6897c3
Hanyu Xiao <hanyu.xiao@gmail.com>
2017-05-25 14:35:59 -0700

    Added Ng1Ng2UrlHandlingStrategy to handle both ng1 & 2 routers

--- app/app.config.js
+++ app/app.config.js
@@ -7,12 +7,8 @@ angular.
       $locationProvider.hashPrefix('!');
 
       $routeProvider.
-        when('/phones', {
-          template: '<phone-list></phone-list>'
-        }).
         when('/phones/:phoneId', {
           template: '<phone-detail></phone-detail>'
-        }).
-        otherwise('/phones');
+        });
     }
   ]);
--- app/upgrade/app-routing.module.ts
+++ app/upgrade/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, UrlHandlingStrategy, UrlTree } from '@angular/router';
+import { APP_BASE_HREF, HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { PhoneListComponent } from './phone-list/phone-list.component';
+
+export class Ng1Ng2UrlHandlingStrategy implements UrlHandlingStrategy {
+  shouldProcessUrl(url: UrlTree) {
+    return url.toString() === '/' || url.toString() === '/phones';
+  }
+  extract(url: UrlTree) { return url; }
+  merge(url: UrlTree, whole: UrlTree) { return url; }
+}
 
 const routes: Routes = [
     { path: '', redirectTo: 'phones', pathMatch: 'full' },
@@ -10,6 +19,11 @@ const routes: Routes = [
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+    exports: [RouterModule],
+    providers: [
+      { provide: APP_BASE_HREF, useValue: '!' },
+      { provide: LocationStrategy, useClass: HashLocationStrategy },
+      { provide: UrlHandlingStrategy, useClass: Ng1Ng2UrlHandlingStrategy }
+    ]
 })
 export class AppRoutingModule { }
